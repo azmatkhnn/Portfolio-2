e4b509783307f1fe5d82a7fec639860bd21fb010
Azmat Khan <azmatkhan.smartkhan.007@gamil.com>
2023-12-15 16:22:42 +0530

    portfolio-2

---
+++ index.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    distance: '20px',
+    duration: 2500,
+    reset: true
+})
+
+ScrollReveal().reveal('.name',{delay: 200, origin: 'left'})
+ScrollReveal().reveal('.right',{delay:400, origin: 'top' })
+ScrollReveal().reveal('.leftSection',{delay:500, origin: 'left' })
+ScrollReveal().reveal('.btn',{delay:600, origin: 'right' })
+ScrollReveal().reveal('.rightSection',{delay:550, origin: 'right' })
+
+
